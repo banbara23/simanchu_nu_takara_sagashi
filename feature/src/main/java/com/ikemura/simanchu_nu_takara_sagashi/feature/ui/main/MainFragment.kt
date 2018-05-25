4e435638d36ce11c7e6ed6e6f7806438718d2b12
ikemura <banbara.work@gmail.com>
2018-05-25 19:03:57 +0900

    公式を参考に修正 https://developer.android.com/jetpack/docs/getting-started

--- feature/src/main/java/com/ikemura/simanchu_nu_takara_sagashi/feature/ui/main/MainFragment.kt
+++ feature/src/main/java/com/ikemura/simanchu_nu_takara_sagashi/feature/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ikemura.simanchu_nu_takara_sagashi.feature.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -24,7 +25,8 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        val text = viewModel.data.value
+        message.text = text
     }
 
 }
--- feature/src/main/java/com/ikemura/simanchu_nu_takara_sagashi/feature/ui/main/MainViewModel.kt
+++ feature/src/main/java/com/ikemura/simanchu_nu_takara_sagashi/feature/ui/main/MainViewModel.kt
@@ -1,7 +1,16 @@
 package com.ikemura.simanchu_nu_takara_sagashi.feature.ui.main
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private val _data = MutableLiveData<String>()
+    val data: LiveData<String>
+        get() = _data
+
+    init {
+        _data.value = "Hello, Jetpack!"
+    }
 }
